4c9e4f4480aa71f83f1f8c1e0cbc9141ef9a01e6
Eduardo Herrera <eduardoj.herrerac@gmail.com>
2021-09-20 16:12:48 -0500

    GNewsApi
    Proyecto ApiGNews para Heroku

---
+++ apiGNews/apiGNews/Controllers/GNewsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using apiGNews.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiGNews.Controllers
+{
+    [Route("api/[controller]")]
+    public class GNewsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                Posts posts = new Posts();
+                List<miniPost> miniPost = new List<miniPost>();
+                var url = "https://gnews.io/api/v4/search?q=watches&token=a54fda58ab820ab0d2798f2e1c6349d0";
+                using (var http = new HttpClient())
+                {
+                    var response = await http.GetStringAsync(url);
+                    posts = JsonConvert.DeserializeObject<Posts>(response);
+                    foreach (var item in posts.Post)
+                    {
+                        miniPost.Add(new Models.miniPost
+                        {
+                            Title = item.Title,
+                            Description = item.Description,
+                            hide = true,
+                            Img = item.Image
+                        });
+                    }
+
+                }
+                return Ok(miniPost);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ apiGNews/apiGNews/Models/Post.cs
@@ -0,0 +1,90 @@
+﻿namespace apiGNews.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Posts
+    {
+        [JsonProperty("totalArticles")]
+        public long TotalArticles { get; set; }
+
+        [JsonProperty("articles")]
+        public Post[] Post { get; set; }
+    }
+
+    public partial class Post
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("image")]
+        public Uri Image { get; set; }
+
+        [JsonProperty("publishedAt")]
+        public DateTimeOffset PublishedAt { get; set; }
+
+        [JsonProperty("source")]
+        public Source Source { get; set; }
+    }
+
+    public partial class Source
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+
+    public partial class Post
+    {
+        public static Post FromJson(string json) => JsonConvert.DeserializeObject<Post>(json, apiGNews.Models.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Post self) => JsonConvert.SerializeObject(self, apiGNews.Models.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    public partial class miniPost
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("image")]
+        public Uri Img { get; set; }
+
+        [JsonProperty("hide")]
+        public bool hide { get; set; }
+    }
+}
+
